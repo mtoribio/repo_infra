4a09493994d1869a3c4406374b994d8f4b0d835a
AlexisToribio <alexistoba21@hotmail.com>
2024-02-19 15:28:13 -0500

    feat: add infrastructure file to call stacks

--- bin/infraestructura.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { InfraestructuraStack } from '../lib/infraestructura-stack';
-
-const app = new cdk.App();
-new InfraestructuraStack(app, 'InfraestructuraStack');
---
+++ bin/infrastructure.ts
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { createName } from '../utils/createName';
+import { NetworkStack } from '../lib/network-stack';
+import { DataBaseStack } from '../lib/db-stack';
+import { RepositoryStack } from '../lib/repository-stack';
+import { ContainerStack } from '../lib/container-stack';
+import { DistributionStack } from '../lib/distribution-stack';
+import { CertificateStack } from '../lib/certificate-stack';
+import { SandBoxStack } from '../lib/sandbox-stack';
+import { BucketStack } from '../lib/bucket-stack';
+import { EmailStack } from '../lib/email-stack';
+import { dev as env } from './environments';
+
+const app = new cdk.App();
+
+const { project } = env;
+
+const networkStack = new NetworkStack(app, createName('stack', 'network'), {
+	env,
+});
+const databaseStack = new DataBaseStack(app, createName('stack', 'database'), {
+	env,
+	vpc: networkStack.vpc,
+	dbSG: networkStack.dbSG,
+	redisSG: networkStack.elasticCacheSG,
+	subnetGroup: networkStack.subnetGroup,
+});
+const repositoryStack = new RepositoryStack(app, createName('stack', 'repository'), { env });
+const acmUsEast1Stack = new CertificateStack(app, 'hrmgo-us-east-1-stack-dev-acm', {
+	env: { ...env, region: 'us-east-1' },
+	crossRegionReferences: true,
+});
+const acmUsEast2Stack = new CertificateStack(app, createName('stack', 'acm'), {
+	env,
+});
+const containerStack = new ContainerStack(app, createName('stack', 'container'), {
+	env,
+	vpc: networkStack.vpc,
+	containerSG: networkStack.containerSG,
+	repository: repositoryStack.repository,
+	albSG: networkStack.albSG,
+	hrmgoCertificate: acmUsEast2Stack.hrmgoCertificate,
+});
+const distributionStack = new DistributionStack(app, createName('stack', 'distribution'), {
+	env,
+	vpc: networkStack.vpc,
+	alb: containerStack.alb,
+	hrmgoCertificate: acmUsEast1Stack.hrmgoCertificate,
+	crossRegionReferences: true,
+});
+const bucketStack = new BucketStack(app, createName('stack', 'bucket'), {
+	env,
+});
+const sandboxStack = new SandBoxStack(app, createName('stack', 'sandbox'), {
+	env,
+	vpc: networkStack.vpc,
+	bastionHostSG: networkStack.bastionHostSG,
+});
+const emailStack = new EmailStack(app, createName('stack', 'email'), { env });
+
+cdk.Tags.of(networkStack).add('proyecto', project);
+cdk.Tags.of(databaseStack).add('proyecto', project);
+cdk.Tags.of(repositoryStack).add('proyecto', project);
+cdk.Tags.of(containerStack).add('proyecto', project);
+cdk.Tags.of(distributionStack).add('proyecto', project);
+cdk.Tags.of(acmUsEast1Stack).add('proyecto', project);
+cdk.Tags.of(acmUsEast2Stack).add('proyecto', project);
+cdk.Tags.of(bucketStack).add('proyecto', project);
+cdk.Tags.of(emailStack).add('proyecto', project);
+cdk.Tags.of(sandboxStack).add('proyecto', project);
