338f474a6c34a11817d1cb5e086d1d6f41157971
AlexisToribio <alexistoba21@hotmail.com>
2024-02-19 15:23:19 -0500

    feat: add certificate stack

---
+++ lib/certificate-stack.ts
@@ -0,0 +1,36 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as ssm from "aws-cdk-lib/aws-ssm";
+
+export interface CertificateStackProps extends StackProps {
+    env: {
+        region: string;
+        project: string;
+        environment: string;
+        domainName: string;
+    };
+}
+
+export class CertificateStack extends Stack {
+    public readonly hrmgoCertificate: acm.ICertificate;
+
+    constructor(scope: Construct, id: string, props: CertificateStackProps) {
+        super(scope, id, props);
+
+        const parameterName = `hrmgo-${props.env.region}-ps-${props.env.environment}-certificate-arn`;
+
+        const certificateArn = ssm.StringParameter.fromStringParameterName(
+            this,
+            "lambdaRoleArn",
+            parameterName
+        ).stringValue;
+
+        this.hrmgoCertificate = acm.Certificate.fromCertificateArn(
+            this,
+            "Certificate",
+            certificateArn
+        );
+    }
+}
