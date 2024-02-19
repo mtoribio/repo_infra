069ae9d9fbf6ee6a97338b5b29b4600717bee083
AlexisToribio <alexistoba21@hotmail.com>
2024-02-19 15:20:57 -0500

    feat: add repository stack

---
+++ lib/repository-stack.ts
@@ -0,0 +1,23 @@
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { createName } from "../utils/createName";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+
+export interface RepositoryStackProps extends StackProps {
+    env: { region: string; project: string; environment: string };
+}
+
+export class RepositoryStack extends Stack {
+    public readonly repository: ecr.Repository;
+
+    constructor(scope: Construct, id: string, props: RepositoryStackProps) {
+        super(scope, id, props);
+
+        // Elastic Container Repository (AWS ECR)
+        this.repository = new ecr.Repository(this, "ECRRepository", {
+            repositoryName: createName("ecr", "repository"),
+            emptyOnDelete: true,
+            removalPolicy: RemovalPolicy.DESTROY,
+        });
+    }
+}
