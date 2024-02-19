07df99eda514c91b4c09bd7da9d0dab50d077786
AlexisToribio <alexistoba21@hotmail.com>
2024-02-19 15:20:17 -0500

    feat: setup infrastructure project

---
+++ bin/environments.ts
@@ -0,0 +1,19 @@
+export const dev = {
+    region: "us-east-2",
+    project: "hrmgo",
+    environment: "dev",
+    dbName: "hrm_go",
+    dbUser: "root",
+    domainName: "hrmgotest.overall.pe",
+    email: "notificaciones@overall.pe",
+};
+
+export const prod = {
+    region: "us-east-2",
+    project: "hrmgo",
+    environment: "prod",
+    dbName: "hrm_go",
+    dbUser: "root",
+    domainName: "hrmgotest.overall.pe",
+    email: "notificaciones@overall.pe",
+};
