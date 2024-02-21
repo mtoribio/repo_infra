8f333926bc2c52b5b1dff6fe98c42b6cf868b10f
AlexisToribio <alexistoba21@hotmail.com>
2024-02-21 10:58:46 -0500

    refactor: change and follow rules of eslint

--- lib/certificate-stack.ts
+++ lib/certificate-stack.ts
@@ -1,36 +1,31 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
-import * as route53 from "aws-cdk-lib/aws-route53";
-import * as acm from "aws-cdk-lib/aws-certificatemanager";
-import * as ssm from "aws-cdk-lib/aws-ssm";
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 export interface CertificateStackProps extends StackProps {
-    env: {
-        region: string;
-        project: string;
-        environment: string;
-        domainName: string;
-    };
+	env: {
+		region: string;
+		project: string;
+		environment: string;
+		domainName: string;
+	};
 }
 
 export class CertificateStack extends Stack {
-    public readonly hrmgoCertificate: acm.ICertificate;
+	public readonly hrmgoCertificate: acm.ICertificate;
 
-    constructor(scope: Construct, id: string, props: CertificateStackProps) {
-        super(scope, id, props);
+	constructor(scope: Construct, id: string, props: CertificateStackProps) {
+		super(scope, id, props);
 
-        const parameterName = `hrmgo-${props.env.region}-ps-${props.env.environment}-certificate-arn`;
+		const parameterName = `hrmgo-${props.env.region}-ps-${props.env.environment}-certificate-arn`;
 
-        const certificateArn = ssm.StringParameter.fromStringParameterName(
-            this,
-            "lambdaRoleArn",
-            parameterName
-        ).stringValue;
+		const certificateArn = ssm.StringParameter.fromStringParameterName(
+			this,
+			'CertificateARN',
+			parameterName
+		).stringValue;
 
-        this.hrmgoCertificate = acm.Certificate.fromCertificateArn(
-            this,
-            "Certificate",
-            certificateArn
-        );
-    }
+		this.hrmgoCertificate = acm.Certificate.fromCertificateArn(this, 'Certificate', certificateArn);
+	}
 }
